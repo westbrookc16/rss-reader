ca36fb570d6e5a16d99e5396af98ca84f9fbee0d
Chris Westbrook <westbchris@gmail.com>
2021-08-02 13:01:52 -0400

    Got feeds displaying.

--- routes/backgroundFetch.js
+++ routes/backgroundFetch.js
@@ -6,38 +6,56 @@ backgroundRouter.get("/", async (req, res) => {
   const { Pool } = require("pg");
   try {
     const pool = new Pool();
-    await pool.connect();
+    pool.connect();
     const feeds = await pool.query(
-      "select id, url from feeds where url is not null"
+      "select id, url from feeds where url is not null",
+      []
     );
+
     console.log(`count=${feeds.rowCount}`);
     feeds.rows.forEach(async (row) => {
       //update all current feeds to 0 so that we can compare newly entered items against them
       console.log(`url=${row["url"]}`);
-      await pool.query("update items set isNew=false where feedID=$1", [
-        row["id"],
-      ]);
-      //get items and insert them
+      try {
+        const updateRes = await pool.query(
+          "update items set isNew=false where feedID=$1",
+          [row["id"]]
+        );
+
+        //get items and insert them
+
+        console.log("good");
+      } catch (e) {
+        console.log(e.stack);
+      }
       const rssFeed = await parser.parseURL(row["url"]);
       rssFeed.items.forEach(async (item) => {
-        const { title, link, content } = item;
-
-        await pool.query(
-          "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
-          [title, content, link, row["id"]]
-        );
+        try {
+          const { title, link, content } = item;
+          console.log(title);
+          await pool.query(
+            "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
+            [title, content, link, row["id"]]
+          );
+        } catch (e) {
+          console.log(e.stack);
+        }
       }); //ending items foreach
       //delete old items here.
       console.log(`deleteedID=${row["id"]}`);
       pool.query(
         "delete from items where url in (select url from items where feedid=$1 and isNew=false) and isNew=true",
-        [row["id"]]
+        [row["id"]],
+        (err, res) => {
+          if (err) {
+            console.log(err.stack);
+          }
+        }
       );
     });
-    await pool.end();
-    res.send('{"msg":"Feed entered successfully."}');
+    pool.end();
   } catch (e) {
-    console.log(e);
+    console.log(e.stack);
     res.send(e);
   }
 });
