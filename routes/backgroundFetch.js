0103cad40bf3056d61389c2b23371f2f4182509d
Chris Westbrook <westbchris@gmail.com>
2021-08-02 20:14:38 -0400

    Fixed refreshing of feeds and moved things around.

--- routes/backgroundFetch.js
+++ routes/backgroundFetch.js
@@ -8,52 +8,42 @@ backgroundRouter.get("/", async (req, res) => {
     const pool = new Pool();
     pool.connect();
     const feeds = await pool.query(
-      "select id, url from feeds where url is not null",
-      []
+      "select id, url from feeds where url is not null"
     );
 
     console.log(`count=${feeds.rowCount}`);
-    feeds.rows.forEach(async (row) => {
+    for (const row of feeds.rows) {
       //update all current feeds to 0 so that we can compare newly entered items against them
       console.log(`url=${row["url"]}`);
-      try {
-        const updateRes = await pool.query(
-          "update items set isNew=false where feedID=$1",
-          [row["id"]]
-        );
 
-        //get items and insert them
+      const updateRes = await pool.query(
+        "update items set isNew=false where feedID=$1",
+        [row["id"]]
+      );
 
-        console.log("good");
-      } catch (e) {
-        console.log(e.stack);
-      }
+      //get items and insert them
+
+      console.log("good");
+
       const rssFeed = await parser.parseURL(row["url"]);
-      rssFeed.items.forEach(async (item) => {
-        try {
-          const { title, link, content } = item;
-          console.log(title);
-          await pool.query(
-            "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
-            [title, content, link, row["id"]]
-          );
-        } catch (e) {
-          console.log(e.stack);
-        }
-      }); //ending items foreach
+      for (const item of rssFeed.items) {
+        const { title, link, content } = item;
+
+        await pool.query(
+          "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
+          [title, content, link, row["id"]]
+        );
+      } ///ending items foreach
       //delete old items here.
       console.log(`deleteedID=${row["id"]}`);
-      pool.query(
+      await pool.query(
         "delete from items where url in (select url from items where feedid=$1 and isNew=false) and isNew=true",
-        [row["id"]],
-        (err, res) => {
-          if (err) {
-            console.log(err.stack);
-          }
-        }
+        [row["id"]]
       );
-    });
+    } //ending for loop for feeds
     pool.end();
+    console.log("success");
+    res.send(`{"success":"true"}`);
   } catch (e) {
     console.log(e.stack);
     res.send(e);
