2e531822f3993e86e6c0c86f1e14225d7afd6829
Chris Westbrook <westbchris@gmail.com>
2021-08-05 16:18:26 -0400

    Fixing index file.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const rssRoute = require("./routes/rss");
 const BackgroundFetch = require("./routes/backgroundFetch");
 const items = require("./routes/items");
 app.use(bodyParser.json());
+
+app.use("/api/feeds", feeds);
+app.use("/api/rss", rssRoute);
+app.use("/api/backgroundFetch", BackgroundFetch);
+app.use("/api/items", items);
+app.use("/api/users", users);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("front/build"));
 
@@ -19,11 +25,6 @@ if (process.env.NODE_ENV === "production") {
 } else {
   require("dotenv").config();
 }
-app.use("/api/feeds", feeds);
-app.use("/api/rss", rssRoute);
-app.use("/api/backgroundFetch", BackgroundFetch);
-app.use("/api/items", items);
-app.use("/api/users", users);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
