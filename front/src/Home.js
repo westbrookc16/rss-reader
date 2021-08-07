eeb4d5447d28c5f639b2ed7739c83547e68d736d
Chris Westbrook <westbchris@gmail.com>
2021-08-07 15:23:03 -0400

    REmoved profile link from home page.

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <NavLink to="/profile">Profile</NavLink>
       <br />
       Welcome to my fancy rss reader
     </div>
