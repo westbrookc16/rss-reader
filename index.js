c5f854e1eee92e74678348d398e99583176b10c8
Chris Westbrook <westbchris@gmail.com>
2021-08-13 13:18:09 -0400

    added serving of index.html in production for requests that are not api requests.

--- index.js
+++ index.js
@@ -29,7 +29,12 @@ app.use("/api/rss", rssRoute);
 app.use("/api/backgroundFetch", BackgroundFetch);
 app.use("/api/items", items);
 app.use("/api/users", users);
-
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
