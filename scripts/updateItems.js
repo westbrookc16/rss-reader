ad9f75fcebda477a7eb388e30938d91e5282c927
Chris Westbrook <westbchris@gmail.com>
2021-08-14 10:29:31 -0400

    Changed hte way new feeds are inserted into the database.

--- scripts/updateItems.js
+++ scripts/updateItems.js
@@ -17,35 +17,23 @@ module.exports = cron.schedule("*/30 * * * *", async () => {
       "select id, url from feeds where url is not null"
     );
 
-    console.log(`count=${feeds.rowCount}`);
     for (const row of feeds.rows) {
-      //update all current feeds to 0 so that we can compare newly entered items against them
-      console.log(`url=${row["url"]}`);
-
-      const updateRes = await client.query(
-        "update items set isNew=false where feedID=$1",
-        [row["id"]]
-      );
-
-      //get items and insert them
-
-      console.log("good");
+      //get items and update or insert them
 
       const rssFeed = await parser.parseURL(row["url"]);
       for (const item of rssFeed.items) {
         const { title, link, content } = item;
-
-        await client.query(
-          "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
+        const updateRows = await client.query(
+          `update items set title=$1, description=$2 where url=$3 and feedID=$4`,
           [title, content, link, row["id"]]
         );
+        if (updateRows.rowCount === 0) {
+          await client.query(
+            "insert into items (title,description,url,feedid) values ($1,$2,$3,$4)",
+            [title, content, link, row["id"]]
+          );
+        }
       } ///ending items foreach
-      //delete old items here.
-      console.log(`deleteedID=${row["id"]}`);
-      await client.query(
-        "delete from items where url in (select distinct url from items where feedid=$1 and isNew=false) and isNew=true and feedid=$1",
-        [row["id"]]
-      );
     } //ending for loop for feeds
 
     console.log("success");
