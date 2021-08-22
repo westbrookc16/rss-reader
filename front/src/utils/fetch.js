034a640ba0968cd42d3d04dd1b48ae7f7fe66a73
Chris Westbrook <westbchris@gmail.com>
2021-08-21 20:26:29 -0400

    Renamed folder.

--- front/src/utils/fetch.js
+++
@@ -1,9 +0,0 @@
-async function callApi(url, method, data) {
-  const res = await fetch(url, {
-    method: method,
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "Application/JSON" },
-  });
-  return await res.json();
-}
-export { callApi };
